21c30a3400b2792db3fec53f0724bcc0c7785862
Lucas <luke.babers@icloud.com>
2023-12-18 20:56:28 -0600

    Adds index.js code to the models folder

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,20 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+});
 // Categories have many Products
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  foreignKey: 'tag_id',
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  foreignKey: 'product_id',
+});
 
 module.exports = {
   Product,
